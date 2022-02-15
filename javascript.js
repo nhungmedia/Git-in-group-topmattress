674f68c89b96520a3b22ddf09e522b017ad43122
Hubert <ziemnioczek@gmail.com>
2022-02-15 15:18:52 +0100

    trying to push

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
-// window.addEventListener("load", start_screen);
+window.addEventListener("load", start_screen);
 
-// function start_screen() {
-//   document.querySelector("#burger").addEventListener("click", open_menu);
-//   document.querySelector("#burger_2").addEventListener("click", close_menu);
-// }
+function start_screen() {
+  document.querySelector("#burger_1").addEventListener("click", open_menu);
+  document.querySelector("#burgerMenu2").addEventListener("click", close_menu);
+}
 
-// function open_menu() {
-//   document.querySelector("#menu_1").classList.remove("hidden");
-// }
-// function close_menu() {
-//   document.querySelector("#menu_1").classList.add("hidden");
-// }
+function open_menu() {
+  document.querySelector("#burgerMenu2").classList.remove("hidden");
+}
+function close_menu() {
+  document.querySelector("#burgerMenu2").classList.add("hidden");
+}
