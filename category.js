9ca2145fa8ffb654b28cbbad212fd72bd4aab05c
Hubert <ziemnioczek@gmail.com>
2022-02-17 13:08:46 +0100

    nm,

--- category.js
+++ category.js
@@ -58,46 +58,6 @@ function showProduct(product) {
   parent.appendChild(copy);
 }
 
-// function showProduct(product) {
-//   console.log(product);
-//   // soldOut onSale
-//   // grab the template
-//   const template = document.querySelector("#productlistTemplate").content;
-//   document.querySelector("#categoryH2").textContent = query;
-//   document.querySelector(".liCurrent").textContent = query;
-//   // document.querySelector(".littleHeadline p").textContent = `${groupedData} products found`;
-//   // clone it
-//   const copy = template.cloneNode(true);
-//   // change image
-//   copy.querySelector(
-//     ".smallProduct img"
-//   ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-//   copy.querySelector(".smallProduct img").alt = product.productdisplayname;
-//   // change content
-//   copy
-//     .querySelector(".viewitem")
-//     .setAttribute("href", `product.html?id=${product.id}`);
-//   copy.querySelector(
-//     "h3"
-//   ).textContent = `${product.brandname} ${product.productdisplayname}`;
-
-//   if (product.soldout) {
-//     copy.querySelector("article").classList.add("soldOut");
-//   }
-//   if (product.discount) {
-//     copy.querySelector("article").classList.add("onSale");
-//     copy.querySelector(".discount").textContent = `${product.discount}%`;
-//     copy.querySelector(".price").textContent = `DKK ${product.price},-`;
-//     copy.querySelector(".discounted p").textContent = `DKK ${Math.floor(
-//       product.price - product.price * (product.discount / 100)
-//     )},-`;
-//   }
-//   // grab parent
-//   const parent = document.querySelector("#categoryAccessories");
-//   // append
-//   parent.appendChild(copy);
-// }
-
 //      button           button             button
 window.addEventListener("load", start_screen);
 
